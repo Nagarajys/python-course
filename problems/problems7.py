08b90520acf3adf8aa6860bf94004d2a5a283c64
NAGARAJ .YS <nagaraj.kondlur@gmail.com>
2026-07-06 22:12:31 +0530

    7 input -output problems added

---
+++ problems/problems7.py
@@ -0,0 +1,6 @@
+# problem : 7 simple intrest
+principle = float(input("principle amount: "))
+rate = float(input("rate of interest: "))
+time = float(input("time in years: "))
+simple_interest = (principle * rate * time) / 100
+print("Simple Interest is: ", simple_interest)
