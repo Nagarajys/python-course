7671956cb6712a005f2129c6b2089e06e54680d8
NAGARAJ .YS <nagaraj.kondlur@gmail.com>
2026-07-08 23:40:28 +0530

    Add problem 12: character replacement example

---
+++ problems/problems12.py
@@ -0,0 +1,7 @@
+# problem :12 replace charecter
+s = "ksit is a college alloted to me in mock round"
+new_s = s.replace('a', '*') # s is a sentence ksit is a college alloted to me in mock round", hear we replace a by *star
+
+
+print("Original:", s) # eddhu original s but next barodhu
+print("After Replace:", new_s) # replace madadru mele new_s yan barutte replace agi anta
