effb4aea25a5bfd46e32cdae5b0979018725254a
NAGARAJ .YS <nagaraj.kondlur@gmail.com>
2026-07-08 22:20:27 +0530

    problems10.py

--- problems/problems10.py
+++ problems/problems10.py
@@ -1,6 +1,6 @@
 # problem : 10 remove spaces
 s = "ksit engineering college"
-new_s = s.replace(" " , "")
+new_s = s.replace(" " , "") # space between colons represents space between string
 
 print("original:", s)
 print("withoutspace:", new_s)
