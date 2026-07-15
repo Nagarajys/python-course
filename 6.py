a860eddbd532c1d041d067ebc9c4ea77ad793874
NAGARAJ .YS <nagaraj.kondlur@gmail.com>
2026-07-15 21:39:18 +0530

    Add examples for set operations including union, intersection, and difference

--- 6.py
+++ 6.py
@@ -19,3 +19,30 @@ print(combined_tuple)
 #tuple repetation
 repeated_tuple = (1234,99,"nagaraj")*100
 print(repeated_tuple)
+
+##SETS..
+s = {1,2,3}# unordered and unindexed
+print(s)
+print(set[1])
+s = {20,132,3325}
+print(s)
+print(set()) # empty set
+s = set()
+print(type(s))
+s1 = {1,2,3}
+s2 = {3,4,5}
+s3 = {5,6,7}
+print(s1 | s2 | s3) # union
+print(s1 & s2)# intersection
+print(s1 - s2 ) # differnce
+print(s1 - s3)
+s = {1,4,5,6,7,78,9}
+s.add(100)
+print(s)
+s.discard(7) 
+s.pop()
+print(s)
+s = {1,2,3,4,5,6}
+a = s.pop()
+print(a) # we cant guess which element will be removed
+s = {67,87,44}
