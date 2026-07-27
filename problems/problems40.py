a63b795a8e744fc20a6cd9493256935c58f6213d
NAGARAJ .YS <nagaraj.kondlur@gmail.com>
2026-07-27 22:58:52 +0530

    Added 9 -problems on for loops with examples and explanations

---
+++ problems/problems40.py
@@ -0,0 +1,53 @@
+##PROBLEMS ON FOR LOOP :-
+#Problem 1  Print numbers from 1 to 10
+for i in range(1,11):
+    print(i)
+    
+# Problem 2  Print numbers from 10 to 1
+for i in reversed(range(1,11)):
+    print(i)
+    
+
+# Problem 3  Print even numbers from 1 to 20
+for x in range(2,21,2):
+    print(x)
+    
+# Problem 4  Print odd numbers from 1 to 20
+for x in range(1,21,2):
+    print(x)
+    
+#Problem 5
+for i in range(6):
+    i = "python"
+    print(i)
+    
+# Problem 6  Find the sum of numbers from 1 to 10
+total = 0
+for i in range(1, 11):
+    total = total + i # 0+1 , 1 +2 = 3 , 3+3 = 6 eriti run agi add agute
+    # hogute bcz this is loop
+print(total)
+
+# Problem 7  Find the sum of even numbers from 1 to 20
+total = 0 
+for i in range(2, 21, 2):
+    total = total + i
+print(total)
+
+
+# Problem 8  Print the multiplication table of 7
+for i in range(1, 11):
+    print(f"{i}*7 = {i*7}")
+
+# Problem 9  Count how many numbers are there from 1 to 100
+count = 0 
+for i in range(1,101):
+    count = count +1
+print(count)
+
+
+    
+    
+    
+    
+
