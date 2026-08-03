d884c1d267c56ddce9c1ee95e60c1e67ab49f9ec
NAGARAJ .YS <nagaraj.kondlur@gmail.com>
2026-08-03 23:08:41 +0530

    Add new examples of functions and string manipulation in 12.py and problems48.py

---
+++ 12.py
@@ -0,0 +1,63 @@
+# TOPIC- FUNCTIONS BASICS..function in Python is a reusable block of organized code designed to perform a single, specific task. Instead of writing the same code repeatedly, you enclose it in a function, give it a name, and run (call) it whenever needed.
+
+def marriage(boy,girl):#parameters
+    print(f"boy is {boy}")
+    print(f"girl is {girl}")
+    print(f"{boy} married {girl}")
+    
+marriage("nagraj","nandini")# here we calling  , this is positional argument
+# andre boy anno position alli nagaraj edhane nav run madtidvi adhe rithi girl kda
+
+marriage( boy = "vikas" , girl="pooja")# key argument
+''' function heng useful andre mate matte print mado neccesity ella just function call
+madudre aythu'''
+
+
+# example tables
+def tables(num):# yav number tables beku anta hakoke
+    for i in range(1,11):
+        print(f"{num}x{i}= {num*i}")
+
+tables(3)
+tables(28)
+tables(6789)
+
+# deafalt parameter values:
+def loves(boy,girl="girl"):
+    print(f"boy is {boy}")
+    print(f"girl is {girl}")
+    print(f"{boy} married {girl}")
+
+'''parameters elli girl yaru anta gothilla so deafalt 
+agi girl barli anta girl = " girl kododhu'''
+loves("nagraj")
+
+
+# return values from a function
+'''A function can return a value from return keyword, 
+which allows the output of function reused elsewhre'''
+def func(num):
+    return int(str(num)*5)
+
+a = 10000
+b = func(5)
+c = a + b
+
+print(c)
+
+# local variables and global variables
+'''Local variables exist only within the specific function or code block
+where they are declared, whereas global variables are declared outside of 
+all functions and can be accessed from anywhere in the entire program'''
+def variable():
+    x = "raja" # local variable 
+    print(x)
+y = "viki"# global variable
+print(y)
+
+
+
+
+
+
+
---
+++ problems/problems48.py
@@ -0,0 +1,7 @@
+s = input("enter name:")
+print(s.strip())
+# sreip used to remove spaces
+
+s = input("string:")
+print(len(s.replace(" ","")))
+# replace dont count space it count only letters
