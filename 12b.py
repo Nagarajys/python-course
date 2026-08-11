85e56d6c5dcc4915afc557fe23ef272b05d70491
NAGARAJ .YS <nagaraj.kondlur@gmail.com>
2026-08-11 22:14:50 +0530

    Add advanced functions examples - Implemented lambda functions with sorting and basic operations

---
+++ 12b.py
@@ -0,0 +1,21 @@
+#FUNCTIONS ADVANCED FUNCTIONS:
+#1.  LAMBDA FUNCTIONS<<<> :a small, one-line anonymous function that you can create 
+# without giving it a name.Instead of using the standard def keyword, writing a name,
+# and using a return statement, you pack everything into a single line of code
+
+add = lambda a,b : a+b #a+b only one expreesion
+
+print(add(79,8))
+
+double  = lambda z : 2*z
+
+print(double(8000))
+
+students = [
+    {"name": "nagaraj", "marks": 94},
+    {"name": "viki", "marks": 99},
+    {"name": "russel", "marks": 100},
+]
+
+students.sort(key=lambda x: x["marks"], reverse=True)
+print(students)
