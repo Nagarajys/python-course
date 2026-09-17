705faa8caf1dd87daf2a518571b84bc7e5ed87a3
nagaraj ys <nagaraj.kondlur@gmail.com>
2026-09-17 23:24:34 +0530

    encapsulation and abstraction examples

--- 13b.py
+++ 13b.py
@@ -16,18 +16,57 @@ student1.display()
 student2.display()
 
 
-'''Student       → Class
+"""Student       → Class
 Student(...)  → Create object
 __init__()    → Automatically runs
 self          → Current object
 name          → Received value
 self.name     → Current object's name
-self.name=name → Store name inside object'''
+self.name=name → Store name inside object"""
 
 
-'''Student → class
+"""Student → class
 Student("Nagaraj", 18) → creates the object
 student1 → that object-na reference/name
 self → currently created object
 self.name → that object's name data
-self.age → that object's age data'''
+self.age → that object's age data"""
+
+
+## THE 4 PILLARS OF OOPl:
+# 1. ENCAPSULATION:
+class bankaccount:
+    def __init__(self, balance):
+        self.__balance = balance
+
+    def deposit(self, amount):
+        if amount > 0:
+            self.__balance = self.__balance + amount
+        else:
+            print("invalid amount")
+
+    def check_balance(self):
+        return self.__balance
+
+
+account = bankaccount(50000)
+account.deposit(10000)
+print(account.check_balance())
+
+
+class bankaccounts:
+    def __init__(self, balance):
+        self.__balance = balance
+
+    def get_balance(self):
+        return self.__balance
+
+    def set_balance(self, amount):
+        if amount >= 0:
+            self.__balance = amount
+
+
+account = bankaccounts(50000)
+print(account.get_balance())
+account.set_balance(8000000)
+print(account.get_balance())  # must call function here
